3c526467c585ce8e38d071214def9fe22cb1a907
UK <uenokan@gmail.com>
2026-03-13 23:21:35 +0900

    Add nixpkgs.config.permittedInsecurePackages

--- home/flake-module.nix
+++ home/flake-module.nix
@@ -15,6 +15,11 @@
     homeConfigurations."uk@kBook-Pro-24" = inputs.home-manager.lib.homeManagerConfiguration {
       pkgs = inputs.nixpkgs.legacyPackages.aarch64-darwin;
       modules = [
+        {
+          nixpkgs.config.permittedInsecurePackages = [
+            "python3.12-ecdsa-0.19.1"
+          ];
+        }
         ./mbp-2024/default.nix
       ];
       extraSpecialArgs = {inherit inputs;};
